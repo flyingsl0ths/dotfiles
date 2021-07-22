96bd1b6d1bb6d4282ecccc800b1e91219ac6c0a6
flyingsl0ths <randomemail32110@gmail.com>
2021-07-22 13:14:40 -0500

    Updated polybar launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar --config=~/.config/polybar/config.ini  mainbar-i3 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "---" | tee -a /tmp/polybar-left.log
+polybar --config=~/.config/polybar/config.ini  mainbar-left 2>&1 | tee -a /tmp/polybar-left.log & disown
 
-echo "Bars launched..."
+echo "---" | tee -a /tmp/polybar-center.log
+polybar --config=~/.config/polybar/config.ini  mainbar-center 2>&1 | tee -a /tmp/polybar-center.log & disown
+
+echo "---" | tee -a /tmp/polybar-right.log
+polybar --config=~/.config/polybar/config.ini  mainbar-right 2>&1 | tee -a /tmp/polybar-right.log & disown
+
+echo "All bars launched..."
+
