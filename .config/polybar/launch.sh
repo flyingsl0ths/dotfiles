bdf20e8f5b698aeb5c3d70ff19e9219499abf9db
flyingsl0ths <randomemail33110@protonmail.com>
2021-11-08 08:59:31 -0600

    [polybar]: Refactor launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,44 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 launch_split_bar() {
-  echo "---" | tee -a /tmp/polybar-left.log
-  polybar --config=~/.config/polybar/config.ini mainbar-left 2>&1 | tee -a /tmp/polybar-left.log &
-  disown
+    echo "---" | tee -a /tmp/polybar-left.log
+    polybar --config=~/.config/polybar/config.ini mainbar-left 2>&1 | tee -a /tmp/polybar-left.log &
 
-  echo "---" | tee -a /tmp/polybar-center.log
-  polybar --config=~/.config/polybar/config.ini mainbar-center 2>&1 | tee -a /tmp/polybar-center.log &
-  disown
+    echo "---" | tee -a /tmp/polybar-center.log
+    polybar --config=~/.config/polybar/config.ini mainbar-center 2>&1 | tee -a /tmp/polybar-center.log &
 
-  echo "---" | tee -a /tmp/polybar-right.log
-  polybar --config=~/.config/polybar/config.ini mainbar-right 2>&1 | tee -a /tmp/polybar-right.log &
-  disown
+    echo "---" | tee -a /tmp/polybar-right.log
+    polybar --config=~/.config/polybar/config.ini mainbar-right 2>&1 | tee -a /tmp/polybar-right.log &
 }
 
 launch_i3_bar() {
-  echo "---" | tee -a /tmp/polybar-i3.log
-  polybar --config=~/.config/polybar/config.ini i3-bar 2>&1 | tee -a /tmp/polybar-i3.log &
-  disown
+    echo "---" | tee -a /tmp/polybar-i3.log
+    polybar --config=~/.config/polybar/config.ini i3-bar 2>&1 | tee -a /tmp/polybar-i3.log &
 }
 
 launch_xmonad_bar() {
-  echo "---" | tee -a /tmp/polybar-mainbar.log
-  polybar --config=~/.config/polybar/config.ini xmonad-bar 2>&1 | tee -a /tmp/polybar-mainbar.log &
-  disown
+    echo "---" | tee -a /tmp/polybar-mainbar.log
+    polybar --config=~/.config/polybar/config.ini xmonad-bar 2>&1 | tee -a /tmp/polybar-mainbar.log &
 }
 
 launch_bar() {
-  BAR="$1"
+    BAR="$1"
 
-  if [ "$BAR" == "split" ]; then
-    launch_split_bar
-  elif [ "$BAR" == "i3" ]; then
-    launch_i3_bar
-  elif [ "$BAR" == "xmonad" ]; then
-    launch_xmonad_bar
-  fi
+    if [ "$BAR" = "split" ]; then
+        launch_split_bar
+    elif [ "$BAR" = "i3" ]; then
+        launch_i3_bar
+    elif [ "$BAR" = "xmonad" ]; then
+        launch_xmonad_bar
+    fi
 
 }
 
