f2861ecbc145f601479e34bfbb27581e8dfe76f6
flyingsl0ths <randomemail32110@gmail.com>
2021-11-28 22:22:52 -0600

    Add new rofi layouts

--- .config/rofi/launchers/ribbon/launcher.sh
+++ .config/rofi/launchers/ribbon/launcher.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-theme="full_bottom"
-
 dir="$HOME/.config/rofi/launchers/ribbon"
 
-rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$1"
