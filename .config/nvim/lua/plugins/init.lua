7fa71b4887fceccf8aa83a756295d4c9eb3e6e01
flyingsl0ths <randomemail33110@protonmail.com>
2024-11-11 09:01:52 -0800

    Switch markdown plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -156,40 +156,13 @@ local plugins = {
 	"LnL7/vim-nix",
 	"vmchale/dhall-vim",
 	"github/copilot.vim",
-
 	{
-		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-		--   "BufReadPre path/to/my-vault/**.md",
-		--   "BufNewFile path/to/my-vault/**.md",
-		-- },
-		dependencies = {
-			-- Required.
-			"nvim-lua/plenary.nvim",
-
-			-- see below for full list of optional dependencies 👇
-		},
-		opts = {
-			workspaces = {
-				{
-					name = "personal",
-					path = "~/vaults/personal",
-				},
-				{
-					name = "work",
-					path = "~/vaults/work",
-				},
-			},
-
-			-- see below for full list of options 👇
-		},
+		'MeanderingProgrammer/render-markdown.nvim',
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	},
+
 }
 
 local opts = {}
