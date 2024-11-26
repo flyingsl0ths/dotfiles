d47e3e5882a6adc2e3d154f32af4c41e6d304c1c
flyingsl0ths <randomemail33110@protonmail.com>
2024-11-26 05:49:30 -0800

    Update dap config

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -45,42 +45,6 @@ dap.adapters.nlua = function(callback, config)
 	callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
 end
 
-dap.adapters.delve = {
-	type = 'server',
-	port = '${port}',
-	executable = {
-		command = 'dlv',
-		args = { 'dap', '-l', '127.0.0.1:${port}' },
-		-- add this if on windows, otherwise server won't open successfully
-		-- detached = false
-	}
-}
-
--- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-dap.configurations.go = {
-	{
-		type = "delve",
-		name = "Debug",
-		request = "launch",
-		program = "${file}"
-	},
-	{
-		type = "delve",
-		name = "Debug test", -- configuration for debugging test files
-		request = "launch",
-		mode = "test",
-		program = "${file}"
-	},
-	-- works with go.mod packages and sub packages
-	{
-		type = "delve",
-		name = "Debug test (go.mod)",
-		request = "launch",
-		mode = "test",
-		program = "./${relativeFileDirname}"
-	}
-}
-
 local dap_py = require("dap-python")
 dap_py.setup(utils.run_cmd("which python"))
 dap_py.test_runner = "pytest"
