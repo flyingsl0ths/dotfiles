5263f554ec2c2aa4fb3a9a02980a0020aa7c1946
flyingsl0ths <randomemail33110@protonmail.com>
2025-02-18 07:49:00 -0800

    [neovim] Remove pcall

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,10 +1,8 @@
-local autopairs_present, cmp_autopairs = pcall(require,
-	"nvim-autopairs.completion.cmp")
+local cmp_autopairs = require
+    "nvim-autopairs.completion.cmp"
 
-local cmp_present, cmp = pcall(require, "cmp")
+local cmp = require "cmp"
 
-if autopairs_present and cmp_present then
-	cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-	cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
-end
+cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
