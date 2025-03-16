7f840afc47d88cd79f95186501f199c416d364b0
flyingsl0ths <randomemail33110@protonmail.com>
2025-03-15 23:44:56 -0700

    Move fmt autocmds to autocmds.lua

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -24,3 +24,32 @@ vim.api.nvim_create_autocmd({ "BufNew", "BufRead" }, {
 		vim.bo.filetype = "cabal"
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.cabal" },
+	group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true }),
+	desc = "Auto format cabal files",
+	callback = function()
+		local buffer_name = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent write !cabal-fmt -i " .. buffer_name)
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.nix" },
+	group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true }),
+	desc = "Auto format cabal files",
+	callback = function()
+		local buffer_name = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent write !nixfmt " .. buffer_name)
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.sh", "Dockerfile" },
+	group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true }),
+	callback = function()
+		local buffer_name = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent write !shfmt -i " .. buffer_name)
+	end,
+})
