3dacfd775bed8ddbccb6a73db76a9b5b5f93c4ed
flyingsl0ths <randomemail33110@protonmail.com>
2025-03-15 21:55:44 -0700

    Switch to lua api for autocmd(s)

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,4 +1,26 @@
 -- Don't show any numbers inside terminals
-vim.cmd " au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal "
+-- vim.cmd " au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal "
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = { "term://*" },
+	callback = function()
+		vim.cmd(":setlocal nonumber norelativenumber")
+		vim.bo.filetype = "terminal"
+	end,
+})
 
 vim.cmd " au BufNewFile,BufRead *.ejs set filetype=html "
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = { "*.ejs" },
+	callback = function()
+		vim.bo.filetype = "html"
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufNew", "BufRead" }, {
+	pattern = { "*.cabal" },
+	callback = function()
+		vim.bo.filetype = "cabal"
+	end,
+})
--- .config/nvim/lua/plugins/fmt.lua
+++ .config/nvim/lua/plugins/fmt.lua
@@ -1,90 +1,28 @@
-local shfmt = {
-	-- Shell Script Formatter
-	function() return { exe = "shfmt", args = { "-i", 2 }, stdin = true } end
-}
-
-require('formatter').setup({
-	filetype = {
-		zsh = shfmt,
-		sh = shfmt,
-		dockerfile = shfmt,
-
-		cabal = {
-			-- Shell Script Formatter
-			function()
-				return {
-					exe = "cabal-fmt",
-					args = {
-						vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-					},
-					stdin = true
-				}
-			end
-		},
-
-		swift = {
-			function()
-				return {
-					exe = "swift-format",
-					args = {
-						"format",
-						vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-					},
-					stdin = true
-				}
-			end
-		},
-
-		cmake = {
-			function()
-				return {
-					exe = "cmake-format",
-					args = {
-						vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-					},
-					stdin = true
-				}
-			end
-		},
-
-		-- python = {
-		-- 	function()
-		-- 		return {
-		-- 			exe = "autopep8",
-		-- 			args = {
-		-- 				vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-		-- 			},
-		-- 			stdin = true
-		-- 		}
-		-- 	end
-		-- },
-		nix = {
-			function()
-				return {
-					exe = "nixpkgs-fmt",
-					stdin = true,
-				}
-			end
-		},
-		-- haskell = {
-		-- 	function()
-		-- 		return {
-		-- 			exe = "fourmolu",
-		-- 			args = {
-		-- 				"-m",
-		-- 				"stdout",
-		-- 				vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-		-- 			},
-		-- 			stdin = true,
-		-- 		}
-		-- 	end
-		-- },
-	}
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.cabal" },
+	group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true }),
+	desc = "Auto format cabal files",
+	callback = function()
+		local buffer_name = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent write !cabal-fmt -i " .. buffer_name)
+	end,
 })
 
-vim.api.nvim_exec([[
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost *.lua,Dockerfile,*.sh,*.cmake,*.swift,*.md,*.nix,*.cabal FormatWrite
-augroup END
-]], true)
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.nix" },
+	group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true }),
+	desc = "Auto format cabal files",
+	callback = function()
+		local buffer_name = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent write !nixfmt " .. buffer_name)
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.sh", "Dockerfile" },
+	group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true }),
+	callback = function()
+		local buffer_name = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent write !shfmt -i " .. buffer_name)
+	end,
+})
