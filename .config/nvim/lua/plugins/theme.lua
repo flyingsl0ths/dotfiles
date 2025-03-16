1134c729ae8a0834d6bdaf3debb9c30b1f9c1477
flyingsl0ths <randomemail33110@protonmail.com>
2025-03-15 23:46:18 -0700

    Change theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,9 @@
--- local catppuccin = require("catppuccin")
---
--- catppuccin.setup({
--- 	transparent_background = false,
--- 	term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
--- })
+local catppuccin = require("catppuccin")
+
+catppuccin.setup({
+	transparent_background = false,
+	term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+})
 
 vim.cmd.colorscheme "catppuccin-frappe"
 -- vim.cmd.colorscheme "nord"
